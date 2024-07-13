0c9b849b9e19e9541b8eb3c0d894b2db844055e9
Justin Kahn <jdavidkahn@gmail.com>
2024-07-13 15:25:12 -0500

    resolvers

--- index.js
+++ index.js
@@ -3,16 +3,55 @@ import { graphqlHTTP } from 'express-graphql';
 import { buildSchema } from 'graphql';
 import fetch from 'node-fetch';
 
-
-//self signed cert
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
-
+const solarDataStore = [];
 
 const solarSchema = buildSchema(`
   type SolarData {
-  name: String 
+    id: ID!
+    name: String
   }
-  type Query {
-  getSolarData: SolarData}
-  `);
 
+  input SolarDataInput {
+    name: String
+  }
+
+  type Query {
+    getSolarData(id: ID!): SolarData
+    listSolarData: [SolarData]
+    fetchSolarData: SolarData
+  }
+
+  type Mutation {
+    createSolarData(input: SolarDataInput): SolarData
+  }
+`);
+
+const resolvers = {
+  getSolarData: ({ id }) => {
+    for (let i = 0; i < solarDataStore.length; i++) {
+      if (solarDataStore[i].id === id) {
+        return solarDataStore[i];
+      }
+    }
+    return null;
+  },
+  listSolarData: () => {
+    return solarDataStore;
+  },
+  fetchSolarData: async () => {
+    const response = await fetch('http://solarprotocol.net/api/v2/opendata.php?systemInfo=name');
+    const data = await response.json();
+    return {
+      id: "external",
+      name: data.name
+    };
+  },
+  createSolarData: ({ input }) => {
+    const newSolarData = {
+      id: `${solarDataStore.length + 1}`,
+      name: input.name
+    };
+    solarDataStore.push(newSolarData);
+    return newSolarData;
+  }
+};
